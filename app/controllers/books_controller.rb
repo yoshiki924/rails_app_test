c36e9ebc25bc64514e2be9e4c48424a275c72990
yoshiki924 <okamura.yoshiki.0924@gmail.com>
2020-11-23 15:07:46 +0900

    i18n導入

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :price, :memo)
+      params.require(:book).permit(:title, :price, :memo, :book_type)
     end
 end
